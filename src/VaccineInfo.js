556505002b3cf6dbb845056cd363fbf26eca85d2
Sreejit7 <sreejitde7@gmail.com>
2020-11-25 04:16:02 +0530

    Updated Vaccine Info page

--- src/VaccineInfo.js
+++ src/VaccineInfo.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react';
+import {Popup} from 'react-leaflet';
 import './VaccineInfo.css';
 function VaccineInfo() {
   const [vaccineData, setVaccineData] = useState([]);
@@ -17,15 +18,20 @@ function VaccineInfo() {
         <h1>COVID-19 VACCINE INFO</h1>
         <tr>
           <td><h2>Vaccine Name</h2></td>
-          <td><h2>Manufacturers</h2></td>
+          <td><h2 style = {{textAlign:"left",marginLeft:"50px"}}>Manufacturers</h2></td>
           <td><h2>Current Trial Phase</h2></td>
         </tr>
       </div>
       <div className = "vaccine__info">
         {vaccineData.map(({candidate,sponsors,trialPhase}) => (
           <tr>
-            <td><strong style = {{color:"#7d1b07",margin:"auto"}}>{candidate}</strong></td>
-            <td>{sponsors}</td>
+            <td><strong style = {{color:"#7d1b07",margin:"auto"}}>{candidate}</strong>
+            </td>
+            <td>{sponsors.map((item,index) => {
+              return(
+                <li style = {{textAlign:"left",marginLeft:"50px"}} key = {index}>{item}</li>
+              )
+            })}</td>
             <td>{trialPhase}</td>
           </tr>
         ))}
