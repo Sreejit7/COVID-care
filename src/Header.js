52e0be1ff55c88a144dc4800243c7482c7072c04
Sreejit7 <sreejitde7@gmail.com>
2021-03-02 15:57:06 +0530

    Updated styling and image formts for optimization

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import './App.css';
 import Sidebar from './Sidebar';
+import logo from './images/COVID_CARE.webp';
 function Header() {
   return (
     <div className="app__navbar">
         <Sidebar/>
         <Link to = "/">
-          <img className = "app__logo" src = "https://lh3.googleusercontent.com/BjkuCl62-D9rbTLaAd6zC-FvHyaIVYtRhwfyoAdW6XHTXl17pMrRcyvL_6AuYQl3ip4V7_9qIP76gx8fNQxTeZi1Kvn0Rlp2kDxWRGAuWhuUGJhSSwrAwOHtqRnCqowRRKKrPKkLcGobTsZUknDzzQfH7XoMTvA8UK08kzkcHSCdBEkuyWBAPp1-x1-wSziaP5kSiH3FGUP-GshCGb0c_miZuL3WKg0lihH7rjhYM1mJ7ZMZ_9vz9EbVraWDLOeXn6hDw2nfl1W_NLVe-6mGREZof3j7I4unm8PJbDvTSJfbP-dsYISb-S_neIPrGNA2samrOnOaIGfqLC-9XnwPu-okfdkej6MibdFaOY9F2T9eNUOxrGFzEC2R9iBsZNDX9MAqmAOPbCsMp4vFnlC8leZExG1eICa9wK679iNClZ0sT3N1rM9ti6ixUZTIxfZfE3D1FsQpR_J_D_pVXkPWf_hq4_RUQXeHQA5ISiNzt4B7l8xgix09OR9UI3ZS1wgNnUgTK7580QDgxpuXTjQImzlEzymrHqb5sEm6jsy1B0GiXTo08FCyaLLKfGtE-2oQ3BvPFPu7exdXoSWidGh2mj8LKEpM26avhgSisHzcjxujFuwf362EcU2DE6A1mVq8OCqt2wZlRwvlR9IlW2uQWtfhGFkGU413SxTkeZPSdXHl74QSToTtAedXsWpU=w1054-h212-no?authuser=0" alt = ""/>
+        <img className = "app__logo" src = {logo} alt = ""/>
         </Link>
     </div>
   )
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,7 @@ import CloseIcon from '@material-ui/icons/Close';
 import {Link} from 'react-router-dom';
 import {sidebarItems} from "./sidebarItems";
 import './Sidebar.css';
-
+import logo from './images/COVID_CARE.webp';
 function Sidebar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => {
@@ -24,7 +24,7 @@ function Sidebar() {
                         <CloseIcon fontSize='large' style={{color:'#fff'}}/>
                     </Link>
                     <Link to = '/'>
-                        <img className = "sidebar__logo" src = "https://lh3.googleusercontent.com/BjkuCl62-D9rbTLaAd6zC-FvHyaIVYtRhwfyoAdW6XHTXl17pMrRcyvL_6AuYQl3ip4V7_9qIP76gx8fNQxTeZi1Kvn0Rlp2kDxWRGAuWhuUGJhSSwrAwOHtqRnCqowRRKKrPKkLcGobTsZUknDzzQfH7XoMTvA8UK08kzkcHSCdBEkuyWBAPp1-x1-wSziaP5kSiH3FGUP-GshCGb0c_miZuL3WKg0lihH7rjhYM1mJ7ZMZ_9vz9EbVraWDLOeXn6hDw2nfl1W_NLVe-6mGREZof3j7I4unm8PJbDvTSJfbP-dsYISb-S_neIPrGNA2samrOnOaIGfqLC-9XnwPu-okfdkej6MibdFaOY9F2T9eNUOxrGFzEC2R9iBsZNDX9MAqmAOPbCsMp4vFnlC8leZExG1eICa9wK679iNClZ0sT3N1rM9ti6ixUZTIxfZfE3D1FsQpR_J_D_pVXkPWf_hq4_RUQXeHQA5ISiNzt4B7l8xgix09OR9UI3ZS1wgNnUgTK7580QDgxpuXTjQImzlEzymrHqb5sEm6jsy1B0GiXTo08FCyaLLKfGtE-2oQ3BvPFPu7exdXoSWidGh2mj8LKEpM26avhgSisHzcjxujFuwf362EcU2DE6A1mVq8OCqt2wZlRwvlR9IlW2uQWtfhGFkGU413SxTkeZPSdXHl74QSToTtAedXsWpU=w1054-h212-no?authuser=0" alt = ""/>
+                        <img className = "sidebar__logo" src = {logo} alt = ""/>
                     </Link>
                 </li>
                 {sidebarItems.map((item,index) =>{
